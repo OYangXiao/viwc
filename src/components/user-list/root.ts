6b3b2110b6aa1010739b20fdd2fc4edbbecd8fd5
ouyangxiao <oyangxiao@outlook.com>
2021-03-19 20:27:38 +0800

    add lit element version

--- src/components/user-list/root.ts
+++
@@ -1,9 +0,0 @@
-import { reactive } from 'vue';
-
-export type attrType = null | string;
-
-export const attributeNames = ['user-id', 'user-list'];
-export const root = {
-  props: reactive(Object.fromEntries(attributeNames.map((name) => [name, null as attrType]))),
-  instance: undefined,
-};
--- vite.config.ts
+++ vite.config.ts
@@ -1,28 +1,14 @@
-import { defineConfig } from 'vite';
-import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: [
-      { find: '@', replacement: path.resolve(__dirname, '/src') },
-      { find: 'vue', replacement: path.resolve(__dirname, 'node_modules/vue/dist/vue.runtime.esm-browser.js') },
-    ],
-  },
-  clearScreen: false,
-  plugins: [vue()],
   build: {
-    // manifest: true,
-    rollupOptions: {
-      input: {
-        'user-list': path.resolve(__dirname, 'src/components/user-list/user-list.ts'),
-        'user-info': path.resolve(__dirname, 'src/components/user-info/user-info.ts'),
-      },
-      output: {
-        format: 'esm',
-        entryFileNames: '[name].[hash].js',
-      },
+    lib: {
+      entry: 'src/my-element.ts',
+      formats: ['es']
     },
-  },
-});
+    rollupOptions: {
+      external: /^lit-element/
+    }
+  }
+})
